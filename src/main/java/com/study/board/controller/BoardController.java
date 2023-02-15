4ca40fa7399212346a9127a7d77e20149ea56724
sw1104 <sangw1104@gmail.com>
2023-02-16 07:00:11 +0900

    Feat: Board Write 기능 추가

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -1,14 +1,27 @@
 package com.study.board.controller;
 
+import com.study.board.entity.Board;
+import com.study.board.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 @Controller
 public class BoardController {
 
-    @GetMapping("/board/write")
+    @Autowired
+    private BoardService boardService;
+
+    @GetMapping("/board/form")
     public String boardWriteForm() {
         return "boardwrite";
+    }
+
+    @PostMapping("/board/write")
+    public String boardWrite(Board board) {
+        boardService.write(board);
+        return "";
     }
 }
