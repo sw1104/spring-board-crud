b8e0d55e31194bfdec31bb8ea56726d9d5c7a875
sw1104 <sangw1104@gmail.com>
2023-02-16 06:33:54 +0900

    Feat: Board Write Form 추가

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -2,14 +2,13 @@ package com.study.board.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
 
 @Controller
 public class BoardController {
 
-    @GetMapping("/")
-    @ResponseBody
-    public String main() {
-        return "Java Spring Board";
+    @GetMapping("/board/write")
+    public String boardWriteForm() {
+        return "boardwrite";
     }
 }
