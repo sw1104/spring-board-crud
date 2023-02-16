5a428bbc50810b86908bd2a0168fb5b2cefc7d9c
sw1104 <sangw1104@gmail.com>
2023-02-16 12:41:28 +0900

    Feat: Board List form 및 기능 추가

--- src/main/java/com/study/board/service/BoardService.java
+++ src/main/java/com/study/board/service/BoardService.java
@@ -5,6 +5,8 @@ import com.study.board.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class BoardService {
 
@@ -13,5 +15,9 @@ public class BoardService {
 
     public void write(Board board) {
         boardRepository.save(board);
+    }
+
+    public List<Board> boardList() {
+        return boardRepository.findAll();
     }
 }
